d701772a0d03effd6f2ce6057026f1b64504319d
oowada takuji <jiinyo829@gmail.com>
2020-07-28 11:57:06 +0900

    type-script component作成

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { IndexComponent } from './index/index.component';
+import { TypeScriptComponent } from './type-script/type-script.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    IndexComponent
+    IndexComponent,
+    TypeScriptComponent
   ],
   imports: [
     BrowserModule,
